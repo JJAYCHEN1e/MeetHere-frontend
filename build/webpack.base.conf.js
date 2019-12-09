64f3846b8f81a3d5cf729c052bd8dd2b9d82c8ad
JJAYCHEN <31304335+JJAYCHENooo@users.noreply.github.com>
2019-12-09 14:01:46 +0800

    Building issue fixed.
    解决问题：
    ERROR in static/js/0.6ff3495f32691a4b5f09.js from UglifyJs
    Unexpected token: punc (() [./~/_time-formater@1.1.3@time-formater/lib/timeformater.js:21,0][static/js/0.6ff3495f32691a4b5f09.js:41766,10]

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -44,7 +44,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/_time-formater@1.1.3@time-formater/lib/timeformater.js')],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
