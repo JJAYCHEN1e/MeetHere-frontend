471948992b06d9e1e4ea3c58754e0b886f049fcb
mufanx <35735090+mufanx@users.noreply.github.com>
2019-12-09 17:34:05 +0800

    场馆大概布局

---
+++ src/page/stadium.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <head-top></head-top>
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0" style="margin: 50px">
+        <el-card :body-style="{ padding: '0px' }" shadow="hover" style="width: 500px">
+          <img src="./11.png" class="image" />
+          <div style="padding: 14px;">
+            <span>中北自习室</span> <br>
+            <span>中北自习室不允许携带食物入内</span>
+            <div class="bottom clearfix">
+              <el-button type="text" class="button" @click="test">详情</el-button>
+            </div>
+            <div class="bottom clearfix">
+              <el-button type="text" class="button" @click="test">预约</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import headTop from "../components/headTop";
+import visitorPie from "@/components/visitorPie";
+import { getUserCity } from "@/api/getData";
+export default {
+  data() {
+    return {
+      pieData: {}
+    };
+  },
+  components: {
+    headTop,
+    visitorPie
+  },
+  mounted() {
+    this.initData();
+  },
+  methods: {
+    test: function() {
+        alert("you hit me??")
+    }, 
+    async initData() {
+      try {
+        const res = await getUserCity();
+        if (res.status == 1) {
+          this.pieData = res.user_city;
+        } else {
+          throw new Error(res);
+        }
+      } catch (err) {
+        console.log("获取用户分布信息失败", err);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "../style/mixin";
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 500px;
+  height: 500px;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ const vueEdit = r => require.ensure([], () => r(require('@/page/vueEdit')), 'vue
 const adminSet = r => require.ensure([], () => r(require('@/page/adminSet')), 'adminSet');
 const sendMessage = r => require.ensure([], () => r(require('@/page/sendMessage')), 'sendMessage');
 const explain = r => require.ensure([], () => r(require('@/page/explain')), 'explain');
+const stadium = r => require.ensure([], () => r(require('@/page/stadium')), 'stadium');
 
 const routes = [
 	{
@@ -90,6 +91,10 @@ const routes = [
 			path: '/explain',
 			component: explain,
 			meta: ['说明', '说明'],
+		}, {
+			path: '/stadium', 
+			component: stadium,
+			meta: ['场馆', '场馆介绍'],
 		}]
 	}
 ]
