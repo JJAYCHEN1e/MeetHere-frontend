46df3e1ae22fb25069d11427d232e9526ec0a0cb
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-12-12 22:28:14 +0800

    增加用户信息界面

--- src/page/adminSet.vue
+++ src/page/adminSet.vue
@@ -1,128 +1,108 @@
 <template>
     <div class="fillcontain">
         <head-top></head-top>
-        <header class="admin_title">管理员信息</header>
-        <div class="admin_set">
-            <ul>
-                <li>
-                    <span>姓名：</span><span>{{adminInfo.user_name}}</span>
-                </li>
-                <li>
-                    <span>注册时间：</span><span>{{adminInfo.create_time}}</span>
-                </li>
-                <li>
-                    <span>管理员权限：</span><span>{{adminInfo.admin}}</span>
-                </li>
-                <li>
-                    <span>管理员 ID：</span><span>{{adminInfo.id}}</span>
-                </li>
-                <li>
-                    <span>更换头像：</span>
-                    <el-upload
-                      class="avatar-uploader"
-                      :action="baseUrl + '/admin/update/avatar/' + adminInfo.id"
-                      :show-file-list="false"
-                      :on-success="uploadImg"
-                      :before-upload="beforeImgUpload">
-                      <img v-if="adminInfo.avatar" :src="baseImgPath + adminInfo.avatar" class="avatar">
-                      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                    </el-upload>
-                </li>    
-            </ul>
+        <header class="user_title">我的信息</header>
+        <div class="user_set">
+            <el-form
+                :model="userInfoForm"
+                :rules="rules"
+                ref="userInfoForm"
+                label-position="left"
+                label-width="100px"
+            >
+                <el-form-item label="用户 ID">
+                    <span>1</span>
+                </el-form-item>
+                <el-form-item label="邮箱地址">
+                    <span>admin@meethere.com</span>
+                </el-form-item>
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="userInfoForm.username"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号码" prop="phone_number">
+                    <el-input v-model="userInfoForm.phone_number"></el-input>
+                </el-form-item>
+                <el-form-item label="注册时间">
+                    <span>{{ adminInfo.create_time }}</span>
+                </el-form-item>
+            </el-form>
+            <div class="save_button">
+                <el-button
+                    type="success"
+                    icon="el-icon-check"
+                    circle
+                ></el-button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-	import headTop from '../components/headTop'
-    import {mapState} from 'vuex'
-    import {baseUrl, baseImgPath} from '@/config/env'
+import headTop from '../components/headTop'
+import { mapState } from 'vuex'
+import { baseUrl, baseImgPath } from '@/config/env'
 
-    export default {
-        data(){
-            return {
-                baseUrl,
-                baseImgPath,
+export default {
+    data() {
+        return {
+            baseUrl,
+            userInfoForm: {
+                username: 'JJAYCHEN',
+                phone_number: '18066668888'
+            },
+            rules: {
+                username: [
+                    {
+                        required: true,
+                        message: '请输入用户名',
+                        trigger: 'blur'
+                    },
+                    {
+                        min: 1,
+                        max: 8,
+                        message: '长度在 1 到 8 个字符',
+                        trigger: 'blur'
+                    }
+                ],
+                phone_number: [
+                    {
+                        required: true,
+                        pattern: /^1[34578]\d{9}$/, // 正则表达式
+                        message: '目前只支持中国大陆的手机号码',
+                        trigger: 'blur'
+                    }
+                ]
             }
-        },
-    	components: {
-    		headTop,
-    	},
-        computed: {
-            ...mapState(['adminInfo']),
-        },
-        methods: {
-            uploadImg(res, file) {
-                if (res.status == 1) {
-                    this.adminInfo.avatar = res.image_path;
-                }else{
-                    this.$message.error('上传图片失败！');
-                }
-            },
-            beforeImgUpload(file) {
-                const isRightType = (file.type === 'image/jpeg') || (file.type === 'image/png');
-                const isLt2M = file.size / 1024 / 1024 < 2;
-
-                if (!isRightType) {
-                    this.$message.error('上传头像图片只能是 JPG 格式!');
-                }
-                if (!isLt2M) {
-                    this.$message.error('上传头像图片大小不能超过 2MB!');
-                }
-                return isRightType && isLt2M;
-            },
-        },
-    }
+        }
+    },
+    components: {
+        headTop
+    },
+    computed: {
+        // 使用对象展开运算符将此对象混入到外部对象中
+        ...mapState(['adminInfo'])
+    },
+    methods: {}
+}
 </script>
 
 <style lang="less">
-	@import '../style/mixin';
-	.explain_text{
-		margin-top: 20px;
-		text-align: center;
-		font-size: 20px;
-		color: #333;
-	}
-    .admin_set{
-        width: 60%;
-        background-color: #F9FAFC;
-        min-height: 400px;
-        margin: 20px auto 0;
-        border-radius: 10px;
-        ul > li{
-            padding: 20px;
-            span{
-                color: #666;
-            }
-        }
-    }
-    .admin_title{
-        margin-top: 20px;
-        .sc(24px, #666);
-        text-align: center;
-    }
-    .avatar-uploader .el-upload {
-        border: 1px dashed #d9d9d9;
-        margin-top: 10px;
-        border-radius: 6px;
-        cursor: pointer;
-        position: relative;
-        overflow: hidden;
-    }
-    .avatar-uploader .el-upload:hover {
-        border-color: #20a0ff;
-    }
-    .avatar-uploader-icon {
-        font-size: 28px;
-        color: #8c939d;
-        width: 120px;
-        height: 120px;
-        line-height: 120px;
-        text-align: center;
-    }
-    .avatar {
-        width: 120px;
-        height: 120px;
-        display: block;
-    }
+@import '../style/mixin';
+
+.user_set {
+    width: 40%;
+    background-color: #f9fafc;
+    margin: 20px auto 0;
+    border-radius: 10px;
+    padding: 15px;
+}
+.user_title {
+    margin-top: 20px;
+    .sc(24px, #666);
+    text-align: center;
+}
+.save_button {
+    margin: 20px auto 0;
+    text-align: center;
+}
 </style>
