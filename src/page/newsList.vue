492fc3507c08cb086bd4a20456c932c180717f24
XTKKKKK <xtkchina@outook.com>
2019-12-28 21:08:46 +0800

    修复后段新闻页面

--- src/page/newsList.vue
+++ src/page/newsList.vue
@@ -170,7 +170,7 @@ export default {
     async initData() {
       try {
         const res = await getNewsCount({
-          userId: this.adminInfo.AId
+          userId: this.adminInfo.adminId
         })
         if (res.code == 0) {
           this.count = parseInt(res.data['count'])
