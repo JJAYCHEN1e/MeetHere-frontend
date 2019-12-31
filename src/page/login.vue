465dc1bdcfbf7a8bc1d30c9d5305feec4014a26f
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-12-31 11:03:29 +0800

    Update login.vue

--- src/page/login.vue
+++ src/page/login.vue
@@ -63,7 +63,14 @@ export default {
       },
       signUpDialogVisible: false,
       rules: {
-        email: [{ required: true, message: '请输入邮箱地址', trigger: 'blur' }],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          {
+            type: 'email',
+            message: '请输入正确的邮箱地址',
+            trigger: ['blur', 'change']
+          }
+        ],
         password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
       }
     }
