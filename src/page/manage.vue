d76ce3dd39edb4a5f6a5d8ebd18e363716702198
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2020-01-02 22:03:20 +0800

    Update manage.vue

--- src/page/manage.vue
+++ src/page/manage.vue
@@ -14,47 +14,13 @@
           </el-menu-item>
           <el-submenu index="2">
             <template slot="title">
-              <i class="el-icon-document"></i>场馆
+              <i class="el-icon-document"></i>管理
             </template>
             <el-menu-item index="/manage/userList">用户列表</el-menu-item>
             <el-menu-item index="/manage/commentList">留言列表</el-menu-item>
             <el-menu-item index="/manage/bookingList">订单列表</el-menu-item>
             <el-menu-item index="/manage/newsList">新闻管理</el-menu-item>
             <el-menu-item index="/manage/stadiumList">场馆管理</el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-plus"></i>我的
-            </template>
-            <el-menu-item index="/manage/commentList">订单</el-menu-item>
-            <el-menu-item index="/manage/userInfo">个人信息</el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-star-on"></i>场馆
-            </template>
-            <el-menu-item index="/manage/stadium">场馆介绍</el-menu-item>
-            <!-- <el-menu-item index="newMember">用户数据</el-menu-item> -->
-          </el-submenu>
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-edit"></i>编辑
-            </template>
-            <!-- <el-menu-item index="uploadImg">上传图片</el-menu-item> -->
-            <el-menu-item index="/manage/vueEdit">文本编辑</el-menu-item>
-          </el-submenu>
-          <el-submenu index="6">
-            <template slot="title">
-              <i class="el-icon-setting"></i>设置
-            </template>
-            <el-menu-item index="/manage/adminSet">管理员设置</el-menu-item>
-            <!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
-          </el-submenu>
-          <el-submenu index="7">
-            <template slot="title">
-              <i class="el-icon-warning"></i>说明
-            </template>
-            <el-menu-item index="/manage/explain">说明</el-menu-item>
           </el-submenu>
         </el-menu>
       </el-col>
